79458c6f738327c7eb23211c2bd1e4c572a31a90
Attila <attila.marton@evozon.com>
2016-07-13 15:13:25 +0300

    dictionarypage class modified xD

--- src/test/java/dfds/steps/serenity/EndUserSteps.java
+++ src/test/java/dfds/steps/serenity/EndUserSteps.java
@@ -1,12 +1,12 @@
 package dfds.steps.serenity;
 
-import dfds.pages.DictionaryPage;
-import net.thucydides.core.annotations.Step;
-import net.thucydides.core.steps.ScenarioSteps;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
+
+import dfds.pages.DictionaryPage;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
 
 public class EndUserSteps extends ScenarioSteps {
 
